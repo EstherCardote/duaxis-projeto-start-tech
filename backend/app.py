4157f526d6274d0ddb6c8ad8eb38e81cf0acea04
EstherCardote <estheer.correa@hotmail.com>
2026-08-26 21:52:42 -0300

    fix vercel deployment

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-from ml_service import analisar_reposicao
+from .ml_service import analisar_reposicao
 
 
 app = FastAPI()
